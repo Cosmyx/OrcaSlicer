36178e8c63d627b06453f137a8d8a81a493715a5
Noctinae <noctinae@proton.me>
2025-10-20 09:57:41 +0200

    Color replaced in RGB and Hex (again)

--- src/slic3r/GUI/ConnectPrinter.cpp
+++ src/slic3r/GUI/ConnectPrinter.cpp
@@ -53,12 +53,12 @@ ConnectPrinterDialog::ConnectPrinterDialog(wxWindow *parent, wxWindowID id, cons
     m_button_confirm->SetTextColor(wxColour("#FFFFFE"));
 
     StateColor btn_bg(
-        std::pair<wxColour, int>(wxColour(0, 137, 123), StateColor::Pressed),
-        std::pair<wxColour, int>(wxColour(38, 166, 154), StateColor::Hovered),
-        std::pair<wxColour, int>(wxColour(0, 150, 136), StateColor::Normal)
+        std::pair<wxColour, int>(wxColour(188, 79, 250), StateColor::Pressed),
+        std::pair<wxColour, int>(wxColour(115, 79, 182), StateColor::Hovered),
+        std::pair<wxColour, int>(wxColour(162, 0, 255), StateColor::Normal)
     );
 
-    StateColor btn_bd(std::pair<wxColour, int>(wxColour(0, 150, 136), StateColor::Normal));
+    StateColor btn_bd(std::pair<wxColour, int>(wxColour(162, 0, 255), StateColor::Normal));
 
     StateColor btn_text(std::pair<wxColour, int>(wxColour(255, 255, 255), StateColor::Normal));
 
